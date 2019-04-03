710bad5079aea8c54afe0415906e51072a27a6f6
nabla42 <47399809+nabla42@users.noreply.github.com>
2019-04-04 00:12:04 +0300

    Upload Language and SM

---
+++ src/Language.ml
@@ -0,0 +1,212 @@
+(* Opening a library for generic programming (https://github.com/dboulytchev/GT).
+   The library provides "@type ..." syntax extension and plugins like show, etc.
+*)
+open GT 
+open List
+
+(* Opening a library for combinator-based syntax analysis *)
+open Ostap.Combinators
+       
+(* Simple expressions: syntax and semantics *)
+module Expr =
+  struct
+    
+    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
+       notation, it came from GT. 
+    *)
+    @type t =
+    (* integer constant *) | Const of int
+    (* variable         *) | Var   of string
+    (* binary operator  *) | Binop of string * t * t with show
+
+    (* Available binary operators:
+        !!                   --- disjunction
+        &&                   --- conjunction
+        ==, !=, <=, <, >=, > --- comparisons
+        +, -                 --- addition, subtraction
+        *, /, %              --- multiplication, division, reminder
+    *)
+                                                            
+    (* State: a partial map from variables to integer values. *)
+    type state = string -> int 
+
+    (* Empty state: maps every variable into nothing. *)
+    let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
+
+    (* Update: non-destructively "modifies" the state s by binding the variable x 
+      to value v and returns the new state.
+    *)
+    let update x v s = fun y -> if x = y then v else s y
+
+    (* Expression evaluator
+
+          val eval : state -> t -> int
+ 
+       Takes a state and an expression, and returns the value of the expression in 
+       the given state.
+   
+    let eval _ = failwith "Not implemented yet" *)
+    
+    let i2b i = i != 0
+    let b2i b = if b then 1 else 0
+
+    let operator op left right = match op with
+        | "+" -> left + right
+        | "-" -> left - right
+        | "*" -> left * right
+        | "/" -> left / right
+        | "%" -> left mod right
+        | "<" -> b2i(left < right)
+        | ">" -> b2i(left > right)
+        | "<=" -> b2i(left <= right)
+        | ">=" -> b2i(left >= right)
+        | "==" -> b2i(left == right)
+        | "!=" -> b2i(left != right)
+        | "&&" -> b2i((i2b left) && (i2b right))
+        | "!!" -> b2i((i2b left) || (i2b right))
+        | _    -> failwith "Fail with operation"
+        
+
+	let rec eval state expr = match expr with
+		| Const const -> const
+		| Var var -> state var
+		| Binop (op,left, right) -> (operator op) (eval state left) (eval state right)
+
+    (* Expression parser. You can use the following terminals:
+
+         IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
+         DECIMAL --- a decimal constant [0-9]+ as a string
+                                                                                                                  
+    *)
+
+    let parse_bin op = ostap(- $(op)), (fun x y -> Binop (op, x, y))
+    
+    ostap (
+      expr:
+		!(Ostap.Util.expr
+           (fun x -> x)
+           (Array.map (fun (a, ops) -> a, List.map parse_bin ops)
+           [|
+             `Lefta, ["!!"];
+             `Lefta, ["&&"];
+             `Nona, ["<="; "<"; ">="; ">"; "=="; "!="];
+             `Lefta, ["+";"-"];
+             `Lefta, ["*"; "/"; "%"];             
+           |])
+           primary
+         );
+      
+      primary: const:DECIMAL {Const const} | var:IDENT {Var var} | - "(" expr -")"
+      )
+    
+  end
+                    
+(* Simple statements: syntax and sematics *)
+module Stmt =
+  struct
+
+    (* The type for statements *)
+    @type t =
+    (* read into the variable           *) | Read   of string
+    (* write the value of an expression *) | Write  of Expr.t
+    (* assignment                       *) | Assign of string * Expr.t
+    (* composition                      *) | Seq    of t * t 
+    (* empty statement                  *) | Skip
+    (* conditional                      *) | If     of Expr.t * t * t
+    (* loop with a pre-condition        *) | While  of Expr.t * t
+	(* loop with a post-condition       *) | RepeatUntil of t * Expr.t with show
+    
+
+    (* The type of configuration: a state, an input stream, an output stream *)
+    type config = Expr.state * int list * int list 
+
+    (* Statement evaluator
+         val eval : config -> t -> config
+       Takes a configuration and a statement, and returns another configuration
+    
+    let eval _ = failwith "Not implemented yet" *)
+    
+   (* let rec eval (state, input, output) st = match st with
+		| Assign (var, expr) 		-> (Expr.update var (Expr.eval state expr) state, input, output)
+		| Write expr 				-> (state, input, output @ [Expr.eval state expr])
+		| Read var 					-> (Expr.update var (List.hd input) state, List.tl input, output)
+		| Seq (s1, s2) 				-> eval(eval(state, input, output)s1)s2
+		| Skip 						-> (state, input, output)
+		| If (expr, thenSt, elSt) 	-> eval (state, input, output) (if Expr.i2b (Expr.eval state expr) then thenSt else elSt)
+		| While (expr, whSt) 		-> if Expr.i2b (Expr.eval state expr) then eval (eval (state, input, output) whSt) st else (state, input, output)
+		| RepeatUtil (ruSt, expr) 	-> let (stateN, inputN, outputN) = eval (state, input, output) ruSt in
+		if not (Expr.i2b (Expr.eval stateN expr)) then eval (stateN, inputN, outputN) st else (stateN, inputN, outputN);;
+*)
+
+let rec eval cfg stmt: config =
+        let (s, i, o) = cfg in
+        match stmt with
+            | Read x -> (match i with
+                | z :: i_rest -> (Expr.update x z s, i_rest, o)
+                | _           -> failwith "Input read fail")
+            | Write   e             -> (s, i, o @ [Expr.eval s e])
+            | Assign (x, e)         -> (Expr.update x (Expr.eval s e) s, i, o)
+            | Seq    (s1, s2) -> eval (eval cfg s1) s2
+            | Skip                              -> (s, i, o)
+            | If (e, thenStmt, elseStmt)        -> eval (s, i, o) (if Expr.i2b (Expr.eval s e) then thenStmt else elseStmt)
+            | While (e, wStmt)                  -> if Expr.i2b (Expr.eval s e) then eval (eval (s, i, o) wStmt) stmt else (s, i, o)
+            | RepeatUntil (ruStmt, e)           -> let (sNew, iNew, oNew) = eval (s, i, o) ruStmt in
+            if not (Expr.i2b (Expr.eval sNew e)) then eval (sNew, iNew, oNew) stmt else (sNew, iNew, oNew);;
+    (* Statement parser *)
+
+    (* Statement parser *)
+     ostap (
+      simple:
+        "read" "(" x:IDENT ")"         {Read x}
+        | "write" "(" e:!(Expr.expr) ")" {Write e}
+        | x:IDENT ":=" e:!(Expr.expr)    {Assign (x, e)};
+      ifStmt:
+        "if" e:!(Expr.expr) "then" thenBody:parse
+      elifBranches: (%"elif" elifE:!(Expr.expr) %"then" elifBody:!(parse))*
+      elseBranch: (%"else" elseBody:!(parse))?
+        "fi" {
+      	    let elseBranch' = match elseBranch with
+      	        | Some x -> x
+                | None   -> Skip in
+      	            let expandedElseBody = List.fold_right (fun (e', body') else' -> If (e', body', else')) elifBranches elseBranch' in
+      	            If (e, thenBody, expandedElseBody)
+      	     };
+      whileStmt:
+        "while" e:!(Expr.expr) "do" body:parse "od" {While (e, body)};
+      forStmt:
+        "for" initStmt:stmt "," whileCond:!(Expr.expr) "," forStmt:stmt
+        "do" body:parse "od" {Seq (initStmt, While (whileCond, Seq (body, forStmt)))};
+      repeatUntilStmt:
+        "repeat" body:parse "until" e:!(Expr.expr) {RepeatUntil (body, e)};
+      control:
+        ifStmt
+        | whileStmt
+        | forStmt
+      	| repeatUntilStmt
+      	| "skip" {Skip};
+      stmt:
+        simple
+        | control;
+      parse:
+        stmt1:stmt ";" rest:parse {Seq (stmt1, rest)}
+        | stmt
+    )
+      
+  end
+
+(* The top-level definitions *)
+
+(* The top-level syntax category is statement *)
+type t = Stmt.t    
+
+(* Top-level evaluator
+
+     eval : t -> int list -> int list
+
+   Takes a program and its input stream, and returns the output stream
+*)
+let eval p i =
+  let _, _, o = Stmt.eval (Expr.empty, i, []) p in o
+
+(* Top-level parser *)
+let parse = Stmt.parse                                                     
